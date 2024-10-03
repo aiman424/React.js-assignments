b6eb1ede2171ee55436eb57642f37790d1d29215
aiman424 <aimanrizwan424@gmail.com>
2024-10-04 00:41:36 +0500

    done

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Name from "./Name"
+
+
+
+const App = () => {
+  return ( <div>
+    <div>
+      hello World
+      <Name></Name>
+      <Name></Name>
+    </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Name.tsx
@@ -0,0 +1,9 @@
+const Name = () => {
+  return (
+    <div>
+      my name is Aiman
+    </div>
+  )
+}
+
+export default Name
